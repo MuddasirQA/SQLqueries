3239c5194cf78e2d96a1c7867f80701eeacf2cf6
MuddasirQA <77394575+MuddasirQA@users.noreply.github.com>
2021-01-21 10:30:06 +0000

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -6,26 +6,29 @@ public class Hello {
     public static void main(String[] args) {
 
 
-        Results.displayPercentage(Results.displayResults(40,100,150));
+        Results.displayPercentage(Results.displayResults(40, 100, 150));
         Results.displayPercentage(290);
 
 
     }
 
-        public static int add(int a, int b) {
-            return a + b;
-        }
+    public static int add(int a, int b) {
+        return a + b;
+    }
 
-        public static double divide(double a, double b) {
+    public static double divide(double a, double b) {
         return a / b;
     }
+
     public static int multiply(int a, int b) {
         return a * b;
     }
+
     public static int subtract(int a, int b) {
         return a - b;
     }
-public static class Results{
+
+    public static class Results {
 
 
         int phys;
@@ -34,25 +37,22 @@ public static class Results{
         int total;
         int percentage;
 
-        public static int displayResults(int chem, int bio, int phys){
+        public static int displayResults(int chem, int bio, int phys) {
             int total = chem + bio + phys;
-            System.out.println("This person scored " +chem + " In Chemistry, " + bio +" In Bio, and " + phys +" in physics");
+            System.out.println("This person scored " + chem + " In Chemistry, " + bio + " In Bio, and " + phys + " in physics");
             System.out.println("Total result: " + total);
 
 
-
-
-
-return total;
+            return total;
         }
-   public static int  displayPercentage(int score){
 
-            return score * 100  / 450;
+        public static int displayPercentage(int score) {
 
-   }
+            return score * 100 / 450;
+
+        }
 
 
-
-}
+    }
 
 }
